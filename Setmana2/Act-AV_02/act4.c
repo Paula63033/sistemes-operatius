bcf74dc9c5cfb3ea3e8d6f765b6cb5ffa63ff9da
Paula Uber <paula63033@gmail.com>
2021-10-04 18:43:18 +0200

    ok

--- Setmana2/Act-AV_02/act4.c
+++ Setmana2/Act-AV_02/act4.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
  
 typedef struct {
 float x;
 float y;
 } point ;
+
+typedef struct {
+point inici;
+point final;
+} line ;
  
-float dist( point A, point B) {
-return(sqrt((A.x - B.x)*(A.x - B.x) + (A.y - B.y)*(A.y - B.y)));
+float dist( line L ) {
+return(sqrt((L.inici.x- L.final.x)*(L.inici.x - L.final.x) + (L.inici.y - L.final.y)*(L.inici.y - L.final.y)));
 }
  
 int main(){
  
 float d;
-point A, B;
+point A,B;
+line L;
  
 
 printf("The coordinates of the point A are: ");
 scanf("%f %f",&A.x,&A.y);
- 
 
+ 
 printf("\nThe coordinates of the point B are: ");
 scanf("%f %f",&B.x,&B.y);
  
+L.inici = A;
+L.final = B;
 
-printf("\nThe distance between A and B is %f\n", dist(A,B));
+printf("\nThe distance between A and B is %f\n", dist(L));
  
 exit (0);
 }
