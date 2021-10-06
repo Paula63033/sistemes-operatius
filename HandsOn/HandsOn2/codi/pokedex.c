d4795a57b3a3d58772f29b606165cda5d9e01935
Alex Ramon <alexramon3000@gmail.com>
2021-10-06 18:47:25 +0200

    fet a classe

--- HandsOn/HandsOn2/codi/pokedex.c
+++ HandsOn/HandsOn2/codi/pokedex.c
@@ -3,23 +3,40 @@
 #include <unistd.h>
 #include "pokedex.h"
  
-/*FILE* f = fopen("./pokedex.csv", "r");
-while (fgets(buf, 151, f) != NULL) {
-    if ((strlen(buf)>0) && (buf[strlen (buf) - 1] == '\n'))
-    buf[strlen (buf) - 1] = '\0';
-    info = strtok(buf, ",");
-    int id = atoi(info);
-}*/
 
 int add_pokemon(){
-//This method will receive a string literal (line) in the form "id name weight height" and append the line into the pokedex.csv file.
+
+FILE* f = fopen("./pokedex.csv", "r");
+
+    while (fgets(buf, 151, f) != NULL) {
+    if ((strlen(buf)>0) && (buf[strlen (buf) - 1] == '\n')) buf[strlen (buf) - 1] = '\0';
+    /*info = strtok(buf, ",");
+    int id = atoi(info);*/
+    
+
 printf("[add_pokemon]: @NOT IMPLEMENTED \n");
 return EXIT_SUCCESS;
 }
  
-int show_pokemon(){
+int show_pokemon(int position){
 //This method will receive an integer and display information about the pokemon in position "position" in the stdout. 
-printf("[show_pokemon]: @NOT IMPLEMENTED \n");
+
+    /*struct Pokemons{
+
+    }*/
+
+   /* FILE* f = fopen("./pokedex.csv", "r");
+
+    while (fgets(buf, 151, f) != NULL) {
+    if ((strlen(buf)>0) && (buf[strlen (buf) - 1] == '\n')) buf[strlen (buf) - 1] = '\0';
+    info = strtok(buf, ",");
+    int id = atoi(info);
+    
+
+    printf("id: %d, &id");*/
+
+
+printf("[show]: @NOT IMPLEMENTED \n");
 return EXIT_SUCCESS;
 }
  
