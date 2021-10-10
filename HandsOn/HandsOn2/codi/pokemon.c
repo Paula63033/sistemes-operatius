96fd5c5b66008bb2ccfc10dee395790014e95bc5
Alex Ramon <alexramon3000@gmail.com>
2021-10-10 22:32:59 +0200

    refent act2 i 3...

--- HandsOn/HandsOn2/codi/pokemon.c
+++ HandsOn/HandsOn2/codi/pokemon.c
@@ -2,41 +2,36 @@
 #include <math.h>
 #include <stdlib.h>
  
-typedef struct pokemon *Pokemon;
-
 typedef struct {
 int pokemon_id;
 char *name;
 double height;
 double weight;
 
-} line ;
- 
-float dist( line L ) {
-return(sqrt((L.inici.x- L.final.x)*(L.inici.x - L.final.x) + (L.inici.y - L.final.y)*(L.inici.y - L.final.y)));
+} Pokemon;
+
+Pokemon create_pokemon(int pokemon_id, char * name, double height, double weight){
+    Pokemon p;
+    p.pokemon_id = pokemon_id;
+    p.name = name;
+    p.height = height;
+    p.weight = weight;
+
+    return p;
+
 }
  
-int main(){
- 
+int pokemon_id(Pokemon pokemon){
+    return pokemon.pokemon_id;
+}
+char *pokemon_name(Pokemon pokemon){
+    return pokemon.name;
+}
+double pokemon_height(Pokemon pokemon){
+    return pokemon.height;
+}
+double pokemon_weight(Pokemon pokemon){
+    return pokemon.height;
+}
 
-Pokemon new_pokemon(int pokemon_id, char *name, double height,double weight);
-int pokemon_id(Pokemon pokemon);
-char *pokemon_name(Pokemon pokemon);
-double pokemon_height(Pokemon pokemon);
-double pokemon_weight(Pokemon pokemon);
- 
-/*
-printf("The coordinates of the point A are: ");
-scanf("%f %f",&A.x,&A.y);
-
- 
-printf("\nThe coordinates of the point B are: ");
-scanf("%f %f",&B.x,&B.y);
- 
-L.inici = A;
-L.final = B;
-
-printf("\nThe distance between A and B is %f\n", dist(L));
- */
-exit (0);
 }
--- HandsOn/HandsOn2/codi/pokemon.h
+++ HandsOn/HandsOn2/codi/pokemon.h
@@ -1,2 +1,7 @@
 typedef struct pokemon *Pokemon;
 
+Pokemon new_pokemon(int pokemon_id, char *name, double height,double weight);
+int pokemon_id(Pokemon pokemon);
+char *pokemon_name(Pokemon pokemon);
+double pokemon_height(Pokemon pokemon);
+double pokemon_weight(Pokemon pokemon);
