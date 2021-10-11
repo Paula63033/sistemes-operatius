c0e2dabf16c0b6fceb52e1251ff0752883c19f32
Alex Ramon <alexramon3000@gmail.com>
2021-10-11 17:40:23 +0200

    fet act2 i act3 de nou

---
+++ HandsOn/HandsOn2/codi/act3.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int *a, i, j, tmp, n;
+    char msg[250];
+
+    sprintf(msg, "Introdueix el num d'elements a l'array: \n");
+    write(1, msg, strlen(msg));
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        sprintf(msg, "Introdueix un element de l'array: \n");
+        write(1, msg, strlen(msg));
+        scanf("%d", a + i);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+
+            if (*(a + i) > *(a + j))
+            {
+                tmp = *(a + i);
+                *(a + i) = *(a + j);
+                *(a + j) = tmp;
+                
+            }
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        sprintf(msg, " a[%d]=%d: \n", i, *(a + i));
+        write(1, msg, strlen(msg));
+    }
+    exit(0);
+}
