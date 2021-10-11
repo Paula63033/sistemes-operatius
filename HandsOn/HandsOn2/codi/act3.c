70e5ecdfb1b5e16f8045fae8674a01e285a5a74b
Alex Ramon <alexramon3000@gmail.com>
2021-10-11 17:42:17 +0200

    ok ex3

--- HandsOn/HandsOn2/codi/act3.c
+++ HandsOn/HandsOn2/codi/act3.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+
     int *a, i, j, tmp, n;
     char msg[250];
 
@@ -11,17 +11,15 @@ int main(int argc, char *argv[])
     write(1, msg, strlen(msg));
     scanf("%d", &n);
 
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++){
         sprintf(msg, "Introdueix un element de l'array: \n");
         write(1, msg, strlen(msg));
         scanf("%d", a + i);
     }
 
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
+    for (i = 0; i < n; i++){
+
+        for (j = 0; j < n; j++) {
 
             if (*(a + i) > *(a + j))
             {
@@ -32,8 +30,7 @@ int main(int argc, char *argv[])
             }
         }
     }
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         sprintf(msg, " a[%d]=%d: \n", i, *(a + i));
         write(1, msg, strlen(msg));
     }
