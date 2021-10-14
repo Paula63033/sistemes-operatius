be6016ed5b7c02d7056c248e9d9b092d2068e91e
Alex Ramon <alexramon3000@gmail.com>
2021-10-14 15:36:35 +0200

    execv_ok

--- HandsOn/HandsOn4/proces_test.c
+++ HandsOn/HandsOn4/proces_test.c
@@ -6,5 +6,5 @@ int main () {
         printf("");
         printf("Hola sóc el procés Pare:  %d \n", getppid());
         printf("Hola sóc el procés Fill:  %d \n", getpid());
-        printf(" ");
+        printf("");
 }
