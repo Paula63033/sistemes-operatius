b59c571296c59c7bda76b5821319b5d1ea6d0202
Alex Ramon <alexramon3000@gmail.com>
2021-10-20 18:11:02 +0200

    BEST

--- HandsOn/HandsOn4/pare_fill_net.c
+++ HandsOn/HandsOn4/pare_fill_net.c
@@ -21,39 +21,35 @@ int num2 = rand()%2;
 //fem que num1 i num2 no puguin tenir el mateix num...
 while(num1 == num2) num2 = rand()%2;
 
-//imprmim els numeros
-printf("NUM1: %d.....NUM2: %d", num1, num2);
-
 int pid, pid2;
 
 pid = fork();
 
-    if (pid == 0) {
-
-        //fill
-        printf("pid 0");
+if (pid == 0) {
+//fill
+    printf("pid 0  \n");
+    fflush( stdout );
 
     pid2 = fork();
 
+    if(pid2 == 0){
+        //nET
+        printf("pid2 == 0 SOC EL NET\n");
+        kill(getpid(), SIGSTOP);
 
-
-    } else if (pid > 0) {
-            printf("pid > 0");
-
-            pid2 = fork();
-            //pare
-
-            wait();
-
-            if(pid2 == 0){
-
-            } else if (pid2 > 0){
-
-
-            }
-
-
+    } else if (pid2 > 0){
+        //PARE NET / FILL PARE
+        printf("pid2 >0 SOC EL FILL (PARE DEL NET)\n");
+        waitpid(-1, 0, WUNTRACED);
     }
+    kill(getpid(), SIGSTOP);
+
+} else if (pid > 0) {
+    printf("pid > 0\n");
+    //pare
+
+    waitpid(-1, 0, WUNTRACED);
+}
 
 exit(0);
 
