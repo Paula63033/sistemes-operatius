ee9445d2d46684f6e0d05fe9406de86423f11b40
Alex Ramon <alexramon3000@gmail.com>
2021-10-21 16:44:06 +0200

    algun canvi als fitxers

--- HandsOn/HandsOn5/pokemon.h
+++ HandsOn/HandsOn5/pokemon.h
@@ -1,9 +1,9 @@
 typedef struct pokemon *Pokemon;
 
-Pokemon new_pokemon(int pokemon_id, char [32]name, char type[2][32], int total, int hp, int attack, int defense, int spAttack, int spDefense, int speed, int generation, int legendary, int seen, int captured);
+Pokemon new_pokemon(int pokemon_id, char *name , char ** type , int total, int hp, int attack, int defense, int spAttack, int spDefense, int speed, int generation, int legendary, int seen, int captured);
 int pokemon_id(Pokemon pokemon);
-char [] pokemon_name(Pokemon pokemon);
-char[][] pokemon_type(Pokemon pokemon);
+char* pokemon_name(Pokemon pokemon);
+char** pokemon_type(Pokemon pokemon);
 int pokemon_total(Pokemon pokemon);
 int pokemon_hp(Pokemon pokemon);
 int pokemon_attack(Pokemon pokemon);
