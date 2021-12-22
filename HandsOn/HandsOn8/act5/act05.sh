20632d3c16b673d01e43a8cba3829488db423591
@alexr-4 <alexramon3000@gmail.com>
2021-12-22 18:54:05 +0100

    casi acabat act6

--- HandsOn/HandsOn8/act5/act05.sh
+++ HandsOn/HandsOn8/act5/act05.sh
@@ -1,4 +1,35 @@
+#!/bin/bash
+
 filename=$1
+
 outputname=$2
+
 N=$(wc -l < "$filename")
-awk -v N=$N -F, '???' $filename > $outputname
+
+awk -v N=$N -F, '\
+
+BEGIN{
+
+    line = 0
+
+}\
+
+{
+
+if (line == 0) { 
+
+    for(i=1;i<=NF;i++){
+        headers[i] = $i
+    }
+
+  } else {
+      for(i=1;i<=NF;i++){
+        printf("%s:%s\n",headers[i],$i)
+    }
+  }
+
+  line++
+
+}' $filename > $outputname
+#headers[0]="#"
+#if ($3=="Dragon" || $4=="Dragon") {
